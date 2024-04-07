96dbd74bc468972d39a3745ae76ffed59ef43f75
Ariel Aguilar <ariel.aguilarit@gmail.com>
2024-04-07 17:09:06 -0400

    setup projects nest and reactjs

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Título de tu API')
+    .setDescription('Descripción de tu API')
+    .setVersion('1.0')
+    .addTag('api') // opcional
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
